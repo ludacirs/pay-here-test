1bdda4086f55ee1032a68aef323541f0f5ae7af8
dani-capa <dani@capa.ai>
2022-09-14 01:23:12 +0900

    feat: 선택한 레포지토리 chip 구현
    유저가 선택한 레포지토리들을 보여주는 Chip 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { useRecoilState } from "recoil";
-import { Box, Tab, Tabs, Typography } from "@mui/material";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { Box, Chip, Stack, Tab, Tabs, Typography } from "@mui/material";
 import tabAtom from "@recoil/tab";
+import bookmarkAtom, { Bookmark } from "@recoil/bookmark";
+import { stringToRGB } from "@lib/string";
+import { setStorageItem } from "@lib/storage";
 
 const Header = () => {
   const [tabValue, setTabValue] = useRecoilState(tabAtom);
+  const bookmarks = useRecoilValue(bookmarkAtom);
 
   const handleChangeTab = (_: React.SyntheticEvent, newValue: number) => {
     setTabValue(newValue);
@@ -35,8 +39,54 @@ const Header = () => {
         <Tab label={"search repository"} />
         <Tab label={"issues"} />
       </Tabs>
+      {!!bookmarks.length && (
+        <Stack
+          justifyContent={"center"}
+          direction="row"
+          spacing={2}
+          padding={2}
+        >
+          {bookmarks.map(({ owner, repo }) => {
+            return (
+              <RepositoryChip key={owner + repo} owner={owner} repo={repo} />
+            );
+          })}
+        </Stack>
+      )}
     </Box>
   );
 };
 
 export default Header;
+
+const RepositoryChip = ({ owner, repo }: Bookmark) => {
+  const setBookmarks = useSetRecoilState(bookmarkAtom);
+
+  const handleDelete = () => {
+    setBookmarks((prev) => {
+      const nextBookmarks = prev.filter(
+        ({ owner: prevOwner, repo: prevRepo }) =>
+          !(prevOwner === owner && prevRepo === repo),
+      );
+
+      setStorageItem("BOOK_MARKED_REPOSITORY", nextBookmarks);
+      return nextBookmarks;
+    });
+  };
+
+  return (
+    <Chip
+      variant="filled"
+      label={
+        <Typography noWrap>
+          {owner}/{repo}
+        </Typography>
+      }
+      onDelete={handleDelete}
+      sx={{
+        bgcolor: `#${stringToRGB(owner + repo)}`,
+        width: "20%",
+      }}
+    />
+  );
+};
---
+++ src/lib/string.ts
@@ -0,0 +1,15 @@
+const hashCode = (str: string) => {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  return hash;
+};
+
+const stringToRGB = (string: string) => {
+  const c = (hashCode(string) & 0x00ffffff).toString(16).toUpperCase();
+
+  return "00000".substring(0, 6 - c.length) + c;
+};
+
+export { stringToRGB };
